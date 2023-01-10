f69d21d5c50ce0b8a9d16c4cd6b27c033104a6c8
Santi Motoc <notvixcy@gmail.com>
2023-01-10 15:15:02 -0500

    restart button added

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def gameover():
     SCREEN.blit(GM, (0, 0))
   
     MENU_MOUSE_POS = pygame.mouse.get_pos()
-    MENU_TEXT = get_font(80).render("GAME OVER", True, "white")
+    MENU_TEXT = get_font(80).render("TRY AGAIN", True, "white")
     MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
     PREV_SCORE_TEXT = get_font(50).render(f"SCORE: {score}", True, (255, 255, 255))
-    #RESTART_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
-                            #text_input="PLAY", font=get_font(50), base_color="white", hovering_color="#999999")
-    QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 400), 
+    RESTART_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 225), 
+                            text_input="RESTART", font=get_font(50), base_color="white", hovering_color="#999999")
+    QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 450), 
                             text_input="QUIT", font=get_font(50), base_color="white", hovering_color="#999999")
   
     SCREEN.blit(MENU_TEXT, MENU_RECT)
@@ -45,7 +45,9 @@ def gameover():
     for button in [QUIT_BUTTON]:
         button.changeColor(MENU_MOUSE_POS)
         button.update(SCREEN)
-        
+    for button in [RESTART_BUTTON]:
+        button.changeColor(MENU_MOUSE_POS)
+        button.update(SCREEN)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -55,9 +57,9 @@ def gameover():
               run = True
               main = False
               return run, main 
-        if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-            pygame.quit()
-            sys.exit()
+            if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+              pygame.quit()
+              sys.exit()
   
     pygame.display.update()
     
