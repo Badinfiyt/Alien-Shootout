7a0075ef8cded1ba01e5e6039f9e1004278d7c26
Badinfiyt <ajinkyaattarde2@gmail.com>
2023-01-18 00:38:37 -0500

    added comments

--- main.py
+++ main.py
@@ -26,23 +26,27 @@ main = True
 run = False
 score = 0
 
-
-def get_font(size): # Returns Press-Start-2P in the desired size
+# Returns Press-Start-2P in the desired size
+def get_font(size):
   """
   Summary: acquires font file from assets subfolder
   Argument:
-    size: 
+    size: this is the size of the font
+  returns:
+    type: this functioon returns the font in the desired size
   """
   return pygame.font.Font("assets/font.ttf", size)
-
 def gameover(menu, main, run, score):
   """
   Summary: initialises the game over screen. It also prints the main menu and 
   Argument:
-    menu: if the value of this is True, the program will be in its running state, as soon as this value is False, the game quits <class 'bool'>
-    main: if the value of this is True, main menu will be shown to the user and will not be shown if it is False <class 'bool'>
+    menu: if the value of this is True, main menu will be shown to the user and will not be shown if it is False <class 'bool'>
+    main: if the value of this is True, the program will be in its running state, as soon as this value is False, the game quits <class 'bool'>
     run: if the value of this is True, the game window will be running and the main menu screen or the game over screen will be shown if the value is False <class 'bool'>
     score: stores the default variable of the player's score <class 'int'>
+  returns:
+    run: returns the value as True if the user wants to play the game <class 'bool'>
+    main: returns the value as False so the main menu isn't shown to the user while they are playing the game <class 'bool'> 
   """
   #sets caption to 'game over' 
   pygame.display.set_caption('Game Over')
@@ -101,13 +105,18 @@ def gameover(menu, main, run, score):
 
 #Main menu system 
 def menu(main, run):
+  """
+  Summary: initialises the main menu screen. Also initializes the buttons, play and quit
+  Argument:
+    main: if the value of this is True, the program will be in its running state, as soon as this value is False, the game quits <class 'bool'>
+    run: if the value of this is True, the game window will be running and the main menu screen or the game over screen will be shown if the value is False <class 'bool'>
+  """
   while main:
     screen.blit(background, (0, 0))
     screen.blit(tvEffect, (0, 0))
   
-    #almost all of this stuff is explained before, use gameover function for ref
+    #customises the main menu with font, a clickable button and a hovering color
     menuMousePos = pygame.mouse.get_pos()
-    #b68f40 line 43 end, if added remove 41 and 42
     menuText = get_font(80).render("ALIEN SHOOTOUT", True, "white")
     menuRect = menuText.get_rect(center=(640, 100))
     prevScoreText = get_font(50).render(f"SCORE: {score}", True, (255, 255, 255))
@@ -119,14 +128,18 @@ def menu(main, run):
     screen.blit(menuText, menuRect)
     screen.blit(prevScoreText, (width - prevScoreText.get_width() - 450, 550))
   
+    #changes colors of the button when hovered over it
     for button in [playButton, quitButton]:
         button.changeColor(menuMousePos)
         button.update(screen)
-        
+
+    #if the quit button is clicked, the game quits    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        #if the play button is pressed, the game window is initialised
         if event.type == pygame.MOUSEBUTTONDOWN:
             if playButton.checkForInput(menuMousePos):
               run = True
@@ -140,18 +153,26 @@ def menu(main, run):
 returnValues = menu(main, run)
 run = returnValues[0]
 main = returnValues[1]
+
+
 #Game run
 if run:
   pygame.display.set_caption('Alien Shootout')
-  #OOP, AJ should explain this part as I am not too familiar with this topic
   class Game:
 
     def __init__(self, main, run):
+      """
+      Summary: initialises the game screen. Assigns the player health and score. It also setups obstacles and aliens
+      Argument:
+        self: 
+        main: if the value of this is True, the program will be in its running state, as soon as this value is False, the game quits <class 'bool'>
+        run: if the value of this is True, the game window will be running and the main menu screen or the game over screen will be shown if the value is False <class 'bool'>
+      """
       # Player setup
       playerSprite = Player((screenWidth / 2,screenHeight),screenWidth, 5)
       self.player = pygame.sprite.GroupSingle(playerSprite)
   
-  		# health and score setup, change when submitting
+  		# health and score setup
       self.lives = 1
       self.liveSurf = pygame.image.load('player.png').convert_alpha()
       self.liveXStartPos = screenWidth - (self.liveSurf.get_size()[0] * 2 + 20)
@@ -176,11 +197,19 @@ if run:
       self.extra = pygame.sprite.GroupSingle()
       self.extraSpawnTime = randint(40,80)
   
-    #defines create obstacle with given parameters, checks for row_index and row, then iterates through to col_index and col
-    #if col = x, sets x var to x_start + col index * block size + offset, likewise for y, just with row and without offset
-    #block variable set to class obstacle.Block with args block_size, position, and x,y
-    #adds block
+    """defines create obstacle with given parameters, checks for row_index and row, then iterates through to col_index and col
+    if col = x, sets x var to x_start + col index * block size + offset, likewise for y, just with row and without offset
+    block variable set to class obstacle.Block with args block_size, position, and x,y
+    adds block"""
     def create_obstacle(self, xStart, yStart,offsetX):
+      """
+      Summary: creates obstacles
+      Argument:
+        self: 
+        xStart: this is the x position of the obstacle <class 'float'>
+        yStart: this is the y position of the obstacle <class 'float'>
+        offsetX: offset of the x value to make sure it is in the middle <class 'float'>
+      """
       for rowIndex, row in enumerate(self.shape):
         for colIndex,col in enumerate(row):
           if col == 'x':
@@ -191,12 +220,30 @@ if run:
   
     #creates multiple obstacles using create obstacle for every x offset in offset
     def create_multiple_obstacles(self,*offset,xStart,yStart):
+      """
+      Summary: creates multiple obstacles
+      Argument:
+        self: 
+        xStart: this is the x position of the obstacle <class 'float'>
+        yStart: this is the y position of the obstacle <class 'float'>
+        offset: offset is the offset of the coordinates to make it so it is in the middle<class 'tuple'>
+      """
       for offsetX in offset:
         self.create_obstacle(xStart,yStart,offsetX)
   
-    #creates aliens using x and y column and row indexes within the range of rows and columns
-    #makes sure to not create aliens outside the given area
+    
     def alien_setup(self,rows,cols,xDistance = 60,yDistance = 48,xOffset = 70, yOffset = 100):
+      """
+      Summary: creates aliens using x and y column and row indexes within the range of rows and columns. Makes sure to not create aliens outside the given area
+      Argument:
+        self: 
+        rows: this is the rows of the aliens <class 'int'>
+        cols: this is the cols of the aliens <class 'int'>
+        xDistance: this has a default value of 60, this is the xdistance of the aliens <class 'int'>
+        yDistance: this has a deafult value of 48, this is the ydistance of the aliens <class 'int'>
+        xOffset = this has a default value of 70, this is the x offset of the aliens <class 'int'>
+        yOffset = this has a default value of 100, this is the y offset of the aliens <class 'int'>
+      """
       for rowIndex, row in enumerate(range(rows)):
         for colIndex, col in enumerate(range(cols)):
           x = colIndex * xDistance + xOffset
@@ -211,8 +258,12 @@ if run:
           #adds aliens
           self.aliens.add(alienSprite)
   
-    #if alien position hits the end of the screen, moves down aliens one row
     def alien_position_checker(self):
+      """
+      Summary: if alien position hits the end of the screen, moves down aliens one row
+      Argument:
+        self: 
+      """
       allAliens = self.aliens.sprites()
       for alien in allAliens:
         if alien.rect.right >= screenWidth:
@@ -222,28 +273,49 @@ if run:
           self.alienDirection = 1
           self.alien_move_down(2)
   
-    #moves down aliens by adding distance to alien.rect.y
+
     def alien_move_down(self,distance):
+      """
+      Summary: moves down aliens by adding distance to alien.rect.y
+      Argument:
+        distance: this variable stores the distance that the alien moves <class 'int'>
+      """
       if self.aliens:
         for alien in self.aliens.sprites():
           alien.rect.y += distance
   
-    #alien shoots laser using Laser class and adds lasers to alien artillery
+
     def alien_shoot(self):
+      """
+      Summary: alien shoots laser using Laser class and adds lasers to alien artillery
+      Argument:
+        self: 
+      """
       if self.aliens.sprites():
         randomAlien = choice(self.aliens.sprites())
         laserSprite = Laser(randomAlien.rect.center,6,screenHeight)
         self.alienLasers.add(laserSprite)
         
-    #not sure entirely what this function is for
+
     def extra_alien_timer(self):
+      """
+      Summary: this make sure the space ship has a delay in its bullet shooting time
+      Argument:
+        self: 
+      """
       self.extraSpawnTime -= 1
       if self.extraSpawnTime <= 0:
         self.extra.add(Extra(choice(['right','left']),screenWidth))
         self.extraSpawnTime = randint(400,800)
   
-    #checks if user collides with laser, if so, removes laser from visuals
+    
     def collision_checks(self, score):
+      """
+      Summary: checks if user collides with laser, if so, removes laser from visuals
+      Argument:
+        self: 
+        score: this is the score that the user has gotten by destroying alien spaceships <class 'int'>
+      """
       # player lasers 
       if self.player.sprite.lasers:
         for laser in self.player.sprite.lasers:
@@ -296,28 +368,49 @@ if run:
             #pygame.quit()
             #sys.exit()
 
-    #displays amount of lives left, shown bottom right of screen
+    
     def display_lives(self):
+      """
+      Summary: displays amount of lives left, shown bottom right of screen 
+      Argument:
+        self: 
+      """
       for live in range(self.lives - 1):
         x = self.liveXStartPos + (live * (self.liveSurf.get_size()[0] + 10))
         screen.blit(self.liveSurf,(x,8))
   
-    #displays score top left of screen
+
     def display_score(self, score):
+      """
+      Summary: displays score top left of screen
+      Argument:
+        self: 
+        score: this is the score that the user has gotten by destroying alien spaceships <class 'int'>
+      """
       scoreSurf = self.font.render(f'score: {score}',False,'white')
       scoreRect = scoreSurf.get_rect(topleft = (10,-10))
       screen.blit(scoreSurf,scoreRect)
   
-    #if no more aliens are on screen, runs victory message and screen (bit buggy, doesn't run 100% of the time)
+
     def victory_message(self):
+      """
+      Summary: if no more aliens are on screen, runs victory message and screen 
+      Argument:
+        self: 
+      """
       if not self.aliens.sprites():
         victorySurf = self.font.render('You won',False,'white')
         victoryRect = victorySurf.get_rect(center = (screenWidth / 2, screenHeight / 2))
         screen.blit(victorySurf,victoryRect)
         menu(main, run)
   
-    #runs main program, updates all functions and includes all necessary variables and functions to run
+    
     def run(self):
+      """
+      Summary: runs main program, updates all functions and includes all necessary variables and functions to run
+      Argument:
+        self: 
+      """
       self.player.update()
       self.alienLasers.update()
       self.extra.update()
@@ -339,21 +432,32 @@ if run:
   
   #runtime class
   class CRT:
-    #initiation, load tv border image and scale tv background with desired width and height amount
     def __init__(self):
+      """
+      Summary: initiation, loads tv border image and scale tv background with desired width and height amount
+        self: 
+      """
       self.tv = pygame.image.load('tv.png').convert_alpha()
       self.tv = pygame.transform.scale(self.tv,(screenWidth,screenHeight))
   
-    #creates lines for crt and draws line on display in black
+    
     def create_crt_lines(self):
+      """
+      Summary: creates lines for crt and draws line on display in black
+        self: 
+      """
       lineHeight = 3
       lineAmount = int(screenHeight / lineHeight)
       for line in range(lineAmount):
         yPos = line * lineHeight
         pygame.draw.line(self.tv,'black',(0,yPos),(screenWidth,yPos),1)
   
-    #makes tv background slightly transparent, giving retro feel
+    
     def draw(self):
+      """
+      Summary: makes tv background slightly transparent, giving retro feel
+        self: 
+      """
       self.tv.set_alpha(randint(75,90))
       self.create_crt_lines()
       screen.blit(self.tv,(0,0))
